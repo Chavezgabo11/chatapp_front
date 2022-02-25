85492319f59d9636b783065ca692e57e589ed858
GABRIEL CHAVEZ <gechave2001@gmail.com>
2022-02-25 13:22:01 -0500

    Adding Class Content Work

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <section id="app">
+    <header>
+      <h1 class="hidden">Welcome to the Chat App</h1>
+      <h2>Chat App with VueCLI, Socket.io and Express</h2>
+    </header>
+    <!-- Router view is the container that accepts components to render per route -->
+    <router-view></router-view>
+  </section>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+header {
+  text-align: center;
+  background-color: #D92C4C;
+  height: 80px;
+  padding: 50px;
+  color: white;
+  box-shadow: 0 2px 10px #ccc;
+}
+
+header {
+  .hidden { display: none; }
+}
+header { 
+  h2 { padding-top: 20px; padding-bottom: 20px; font-size: 2.5em; margin-bottom: 0.8em;}
+}
+</style>
---
+++ src/views/Chat.vue
@@ -0,0 +1,147 @@
+<template>
+    <main>
+        <h1 class="welcome-message">Welcome {{ ChatUserName }} </h1>
+
+        <section id="chat">
+            <!-- the left-hand side of our UI where we see current users -->
+            <section id="chat-users-ui">
+                <h2>Current Users:</h2>
+                <!-- list of users -->
+                <ul id="current-users">
+                    <!-- this will take a sub-component that will render -->
+                    <ChatUsers
+                    v-for="user in users"
+                    :key="user.id"
+                    :username="user.name"
+                    />
+                </ul>
+            </section>
+            <!-- end left hand -->
+            <!-- main chat UI -->
+            <section id="chat-messages-ui">
+                <!-- custom chat message component here -->
+                <ChatMessage
+                    v-for="msg in messages"
+                    :key="msg.id"
+                    :message="msg.message"
+                    :username="msg.user"
+                />
+
+                <section id="text-wrapper">
+                    <textarea @keyup="trySendMessage" id="message" v-model="message" placeholder="What's on your mind?"></textarea>
+
+                    <button 
+                    id="sendMessage"
+                    @click="sendMessage"
+                    :class="{ 'disabled' : canSend }"
+                    :disabled="canSend"
+                    >
+                        <img id="icon-send" :src="require(`@/assets/icon-send.png`)" alt="Icon Send Message">
+                    </button>
+                </section>
+
+            </section>
+        </section>
+    </main>
+</template>
+<script>
+import io from "socket.io-client";
+import vars from "@/env.js";
+import ChatMessage from "@/components/ChatMessage.vue";
+import ChatUsers from "@/components/ChatUsers.vue";
+
+export default {
+    name: "TheChatComponent",
+
+    props: {
+        ChatUserName: String,
+        username: String
+    },
+
+    mounted() {
+        let vm = this;
+
+        this.socket.on("CONNECTED", (id) => {
+            vm.socketID = id;
+
+            vm.broadCastJoined();
+        })
+
+        this.socket.on("JOINED", (data) => {
+            vm.users = data;
+        })
+
+        this.socket.on('MESSAGE', (message) => {
+            // [...]  is a the spread operator
+            // short hand way to add something to an array, or put
+            // 2 arrays together, etc
+            vm.messages = [...vm.messages, message];
+            console.log(message);
+        })
+
+        this.socket.on('SOMEONE_TYPING', (data) => console.log('someone is typing', data));
+    },
+
+    computed: {
+            canSend: function() {
+            return (this.socketID.trim() === "");
+            }
+    },
+
+    data() {
+        return {
+            socketID: '',
+            users: [],
+            message: '',
+            messages: [],
+            CurrentID: 0,
+
+            // set up the socket connection on the frontend 
+            // this connects to the chat service running on port 3000
+            socket: io(vars.basePath, {
+                withCredentials: false,
+                extraHeaders: {
+                    'Access-Control-Allow-Origin' : '*'
+                }
+            })
+        }
+    },
+
+    methods: {
+        sendMessage(){
+            this.socket.emit('SEND_MESSAGE', {user: this.ChatUserName || "Anonymous", message: this.message});
+
+            // empty put the text area and get ready to input a new message
+            this.message = '';
+        },
+        trySendMessage(event) {
+            console.log('typing a message');
+
+            // if the enter is pushed AND canSend has a value, then call the sendMessage method
+            if (event.keyCode === 13 && this.canSend === false) {
+                this.sendMessage();
+            } else {
+                // else fire off teh USER_TYPING event to the Chat Server
+                this.socket.emit('USER_TYPING', { user: this.username || "Anonymous"});
+            }
+        },
+        getNewId() {
+            this.CurrentID++;
+            return  this.CurrentID;
+        }
+        ,
+        broadCastJoined() {
+            this.socket.emit('JOINED_CHAT', { name: this.ChatUserName || 'Anonymous'})
+        }
+    },
+
+    components: {
+        ChatMessage,
+        ChatUsers
+    }
+}
+</script>
+
+<style lang="scss">
+    @import "@/assets/sass/_chat.scss";
+</style>
